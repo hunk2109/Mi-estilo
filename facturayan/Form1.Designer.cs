b5ff5e192fdb33a72b3ebbba6960662877bfbf36
Hector Jose Taveras Rodriguez <hectojo_2109@hotmail.com>
2018-05-04 21:11:08 -0400

    Arreglo de faltas ortograficas

--- facturayan/Form1.Designer.cs
+++ facturayan/Form1.Designer.cs
@@ -258,7 +258,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 13);
             this.label2.TabIndex = 0;
-            this.label2.Text = "Descriccion";
+            this.label2.Text = "Descripcion";
             // 
             // gbx
             // 
